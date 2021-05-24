2bc01055cee937a956265f4642463abcaf45b227
Hamed Aarab <hawmed.a@gmail.com>
2021-05-24 22:23:13 +0430

    added nexstate WithStore mixin

--- src/lib/mixins.js
+++ src/lib/mixins.js
@@ -75,3 +75,29 @@ export const WithDependencyProvider = (Base) =>
       this.#dependencies.set(key, value);
     }
   };
+
+export const WithStore = (Base) =>
+  class WithStore extends WithDependencyConsumer(Base) {
+    #store;
+
+    get store() {
+      return this.#store;
+    }
+
+    stateChangedCallback(_state) {}
+
+    addedCallback() {
+      super.addedCallback();
+      this.#store = this.requestDependency('store');
+    }
+
+    willMountCallback() {
+      super.willMountCallback();
+      this.stateChangedCallback(this.#store.state);
+    }
+
+    mountedCallback() {
+      super.mountedCallback();
+      this.#store.subscribe(this.stateChangedCallback.bind(this), { signal: this.unmountedSignal });
+    }
+  };
