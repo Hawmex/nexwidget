8b7d23ae07de30c7c8277e07c230d86d271f78ea
Hamed Aarab <hawmed.a@gmail.com>
2021-06-08 16:09:27 +0430

    fixed missing import

--- src/lib/directives.js
+++ src/lib/directives.js
@@ -2,6 +2,7 @@
 
 import { directive } from 'lit-html';
 import { Nexbounce } from 'nexbounce';
+import { addPendingTask } from './pending-task.js';
 
 const latestValues = new WeakMap();
 const lazyLoadRenderDebouncer = new Nexbounce();
