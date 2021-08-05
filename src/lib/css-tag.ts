fe5c9192e5491c256d2f8cae63b4c336520c8042
Hawmex <hawmed.a@gmail.com>
2021-08-05 11:51:51 +0430

    fixed bugs with css-tag

--- src/lib/css-tag.ts
+++ src/lib/css-tag.ts
@@ -1,32 +1,15 @@
-export type CSSValue = CSSResult | number;
+declare global {
+  interface CSSStyleSheet {
+    replaceSync(value: string): void;
+  }
+}
 
 export class CSSResult {
-  static #checkValue(value: CSSValue) {
-    if (value instanceof CSSResult) return value.#CSSText;
-    else if (typeof value === 'number') return value;
-    else
-      throw new Error(
-        `Value passed to a "css" function must be a "css" function result or a number.`,
-      );
-  }
-
-  static #refine(strings: TemplateStringsArray, values: CSSValue[]) {
-    return values.reduce(
-      (accumulator, value, index) =>
-        accumulator + CSSResult.#checkValue(value) + strings[index + 1],
-      strings[0],
-    );
-  }
-
-  #CSSText: string;
   #styleSheet: CSSStyleSheet;
 
-  constructor(strings: TemplateStringsArray, values: CSSValue[]) {
-    this.#CSSText = CSSResult.#refine(strings, values);
-
+  constructor(strings: TemplateStringsArray) {
     this.#styleSheet = new CSSStyleSheet();
-    //@ts-ignore
-    this.#styleSheet.replaceSync(this.#CSSText);
+    this.#styleSheet.replaceSync(strings[0]);
   }
 
   get styleSheet() {
@@ -34,5 +17,4 @@ export class CSSResult {
   }
 }
 
-export const css = (strings: TemplateStringsArray, ...values: CSSValue[]) =>
-  new CSSResult(strings, values).styleSheet;
+export const css = (strings: TemplateStringsArray) => new CSSResult(strings).styleSheet;
